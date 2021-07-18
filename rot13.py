5fe4f8eaef0d6f33da4aacd4bb8ac8e0fbfa51d4
Younes Bendahman <54805065+younesbendahman@users.noreply.github.com>
2021-07-19 01:28:59 +0200

    Update

--- rot13.py
+++ rot13.py
@@ -7,22 +7,18 @@ alphabet = s.ascii_lowercase
 for i in range(len(alphabet)):
     if i + 13 >= 26:
         temp = i + 13 - 26
-
     else:
         temp = i + 13
-
     caesar_alphabet[alphabet[i]] = alphabet[temp]
 
 def convert(txt:str):
     txt.lower()
     output = ""
-
     for i in txt:
         if i in alphabet:
             output += caesar_alphabet[i]
         else:
             output += i
-
     return output
 
 if __name__ == "__main__":
